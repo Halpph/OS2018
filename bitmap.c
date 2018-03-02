1a82db262d801cd6c4a5c5e1c53815c6b34141b2
Halpph <cesaretti.1712254@studenti.uniroma1.it>
2018-03-02 21:37:49 +0100

    beginning 2

--- bitmap.c
+++ bitmap.c
@@ -3,11 +3,9 @@
 BitMapEntryKey BitMap_blockToIndex(int num){
 // converts a block index to an index in the array,
 // and a char that indicates the offset of the bit inside the array
-	BitMapEntryKey* res= malloc(sizeof(BitMapEntryKey));
-	res->entry_num=num;
-	int i;
-	for (i=0;i!=1;i++) res->bit_num=i;//scorre l'array fino alla prima posizione libera
-	return BitMapEntryKey;
+	int num_blocco = num/8-1;//posizione nell'array di char
+	int posizione = num%8-1;//n-esimo bit nel char
+	
 }
 
 int BitMap_indexToBlock(int entry, uint8_t bit_num){
@@ -17,23 +15,15 @@ int BitMap_indexToBlock(int entry, uint8_t bit_num){
 	
 }
 
-int BitMap_get(BitMap* bmap, int start, int status
+int BitMap_get(BitMap* bmap, int start, int status ){
 // returns the index of the first bit having status "status"
 // in the bitmap bmap, and starts looking from position start
-	int i=start;
-	for(;i<num_bits;i++){
-		if(bmap->entries[i] == status) return i;//ritorna l'indice se lo trova
-	}
-	return -1;//sennò torna -1
+	
 }
 
 
 
 int BitMap_set(BitMap* bmap, int pos, int status){
 	// sets the bit at index pos in bmap to status
-	if (pos < bmap->num_bits)	{
-		bmap->entries[pos]=status;
-		return 0;//success
-	}
-	else return -1;//fail
+	
 }
