e1ee78f99a2790399068f606d9f88857142a7b71
lim996 <simonestaffa96@gmail.com>
2018-05-15 17:48:16 +0200

    create file completed

--- simplefs.c
+++ simplefs.c
@@ -52,7 +52,7 @@ void SimpleFS_format(SimpleFS* fs){
 	fs->disk->header->free_blocks = fs->disk->header->num_blocks;  //all blocks are free
 	fs->disk->header->first_free_block = 0;
 	int bitmap_size = fs->disk->header->bitmap_entries; //num_blocks entries
-	bzero(fs->disk->bitmap_data, bitmap_size); //put 0 in every bytes for bitmap_size length
+	memset(fs->disk->bitmap_data,'\0', bitmap_size); //put 0 in every bytes for bitmap_size length
 
 	ret = DiskDriver_writeBlock(fs->disk, &rootDir, 0);		//write root directory on block 0
 	if (ret == -1)
@@ -62,6 +62,11 @@ void SimpleFS_format(SimpleFS* fs){
 // creates an empty file in the directory d
 // returns null on error (file existing, no free blocks)
 // an empty file consists only of a block of type FirstBlock
+
+// 3 scenarios:
+// 1) file already exists
+// 2) there's no space in disk
+// 3) everything is ok, just find a free space and write the block
 FileHandle* SimpleFS_createFile(DirectoryHandle* d, const char* filename){
     int ret;
 
@@ -74,13 +79,14 @@ FileHandle* SimpleFS_createFile(DirectoryHandle* d, const char* filename){
         int i;
         for(i = 0; i < entries; i++){
             if(fdb->file_blocks[i] > 0 && (DiskDriver_readBlock(disk_driver,&to_check,file_blocks[i]) != -1)){ //check if block is free
-                if(to_check.fdb.name,filename,128){
+                if(strncmp(to_check.fdb.name,filename,128)){
                     printf("createFile: File already exists!\n");
                     return NULL;
                 }
             }
         }
 
+        // checking file esistence in DirectoryBlock
         int next_block = fdb->header.next_block;
         DirectoryBlock db;
 
@@ -93,7 +99,7 @@ FileHandle* SimpleFS_createFile(DirectoryHandle* d, const char* filename){
             // check file esistence in each block
             for(i = 0; i < entries; i++){
                 if(db.file_blocks[i] > 0 && (DiskDriver_readBlock(disk_driver,&to_check,file_blocks[i]) != -1)){ //check if block is free
-                    if(to_check.fdb.name,filename,128){
+                    if(strncmp(to_check.fdb.name,filename,128)){
                         printf("createFile: File already exists!\n");
                         return NULL;
                     }
@@ -103,7 +109,134 @@ FileHandle* SimpleFS_createFile(DirectoryHandle* d, const char* filename){
         }
     }
 
-    
+    // if I'm here FILE ('filename') doesn't exists
+    // => I can create it!
+    // getting first_free_block index
+    int first_free_block = DiskDriver_getFreeBlock(disk_driver,disk_driver->header,first_free_block);
+    if(first_free_block == -1){
+        printf("ERROR: There are no free blocks!");
+        return NULL;
+    }
+
+    FirstFileBlock* new_file_block = calloc(1,sizeof(FirstFileBlock)); // allocate and fill with 0
+    // initializing header
+    new_file_block->header.block_in_file = 0; // new file hasn't blocks
+    new_file_block->header.next_block = -1; // new file doesn't have next block
+    new_file_block->header.previous_block = -1; // new file doesnt'have previous block
+
+    // insert in directory
+    new_file_block->fcb.directory_block = fdb->fcb.block_in_disk; // first_block in parent directory
+    new_file_block->fcb.block_in_disk = first_free_block; // saving my position in disk
+    new_file_block->fcb.is_dir = 0; // is a file (==0), not a directory (==-1)
+    strncpy(new_file_block->fcb.name,filename,128); // passing filename to fcb.name
+
+    ret = DiskDriver_writeBlock(disk_driver,new_file,first_free_block); // write file on disk in position new_block
+    if(ret == -1){
+        printf("ERROR: cannot create file, problem in write_block!\n");
+        return NULL;
+    }
+
+    int i = 0;
+    int found = 0; // 1 if I found space in an already created block
+    int entry = 0; // index of the file_block space found
+    int where_i_found_space = 0; // 0 space on FDB, 1 space on DB
+    int where_i_save = 0; // 0 saves on FDB, 1 saves on DB
+    int block_position = 0; // used to save index of block in directory
+    int block_number = fdb->fcb.block_in_disk; // used to save current block in disk
+    int max_free_space_in_fdb = (BLOCK_SIZE-sizeof(BlockHeader)-sizeof(FileControlBlock)-sizeof(int))/sizeof(int);
+    int max_free_space_in_db = (BLOCK_SIZE-sizeof(BlockHeader))/sizeof(int);
+
+    if(fdb->num_entries < max_free_space_in_fdb){ // check if there's enough space in FirstDirectoryBlock
+        int* blocks = fdb->file_blocks;
+        for(i = 0; i < max_free_space_in_fdb; i++){
+            if(blocks[i] == 0){
+                found = 1;
+                entry = i;
+                break;
+            }
+        }
+    }else{
+        where_i_found_space = 1; // i'm in DB now
+        // if there isn't space in FirstDirectoryBlock
+        // check in DirectoryBlock
+        DirectoryBlock directory_block;
+        next_block = fdb->header.next_block;
+
+        while(next_block != -1 && found == 0){ // while there's a next block, and while I've not found a free space
+            ret = DiskDriver_readBlock(disk_driver,directory_block,next_block); // reading each block
+            if(ret == -1){
+                printf("createFile: error in readBlock\n");
+                DiskDriver_freeBlock(disk, fdb->fcb.block_in_disk);
+                return NULL;
+            }
+
+            blocks = directory_block.file_blocks;
+            block_position += 1; // change block => update variables
+            block_number = next_block;
+            for(i = 0; i < max_free_space_in_db; i++){ // looping to find free space
+                if(blocks[i] == 0){
+                    found = 1;
+                    entry = i;
+                    break;
+                }
+            }
+            where_i_save = 1;
+            next_block = db.header.next_block;
+
+        }
+
+    }
+
+    if(found == 0){ // if all blocks are full, i've to create a new DirectoryBlock
+        DirectoryBlock new_db = {0};
+        new_db.header.next_block = -1;
+        new_db.header.block_in_file = block;
+        new_db.header.previous_block = block_number;
+        new_db.file_blocks[0] = new_file_block; // populating first block, with the new file created
+
+        int new_dir_block = DiskDriver_getFreeBlock(disk_driver, disk->header->first_free_block);
+        if(new_dir_block == -1){
+            printf("ERROR: cannot create new directory! No free blocks\n");
+            DiskDriver_freeBlock(disk_driver,fdb->fcb.block_in_disk);
+            return NULL;
+        }
+
+        ret = DiskDriver_writeBlock(disk_driver,&new_db,new_dir_block); // write directory block on disk
+        if(ret == -1){
+            printf("ERROR: cannot write directory on disk!\n");
+            DiskDriver_freeBlock(disk_driver,fdb->fcb.block_in_disk);
+            return NULL;
+        }
+
+        if(where_i_save == 0){ // save on FDB
+            fdb->header.next_block = new_dir_block;
+        }else // save on DB
+            directory_block.next_block = new_dir_block;
+        directory_block = new_db;
+        block_number = new_dir_block;
+    }
+
+    if(where_i_found_space == 0){ // space in FDB
+        fdb->num_entries++;
+        fdb->file_block[entry] = new_file_block;
+        DiskDriver_freeBlock(disk_driver,fdb->fcb.block_in_disk);
+        DiskDriver_writeBlock(disk_driver,fdb,fdb.->fcb.block_in_disk);
+    }else{ // space in DB
+        fdb->num_entries++;
+        DiskDriver_freeBlock(disk_driver,fdb->fcb.block_in_disk);
+        DiskDriver_writeBlock(disk_driver,fdb,fdb.->fcb.block_in_disk);
+        directory_block.file_blocks[entry] = new_file_block;
+        DiskDriver_freeBlock(disk_driver,block_number);
+        DiskDriver_writeBlock(disk_driver,&directory_block,block_number);
+    }
+
+    FileHandle* file_handle = malloc(sizeof(FileHandle));
+    file_handle->sfs = d->sfs;
+    file_handle->fcb = new_file_block;
+    file_handle->directory = fdb;
+    file_handle->pos_in_file = 0;
+
+    return fh;
 }
 
 // reads in the (preallocated) blocks array, the name of all files in a directory
